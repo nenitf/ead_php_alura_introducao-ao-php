74e9472de96c8d8b935a5f8fe77f49c05e158211
Felipe Silva <felipe.dacs@gmail.com>
2021-01-05 23:54:45 -0300

    :tv: add aula 13

--- repeticao.php
+++ repeticao.php
@@ -1,8 +1,14 @@
 <?php
 
+// while serve faz mais sentido qnd o loop é indeterminado
+/*
 $contador = 1;
-
 while ($contador <= 15) {
     echo "#$contador" . PHP_EOL;
     $contador = $contador + 1;
+}
+*/
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    echo "#$contador" . PHP_EOL;
 }
