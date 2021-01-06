abc23c6260a9c978ec6d89e601905d5fe3a7f9bf
Felipe Silva <felipe.dacs@gmail.com>
2021-01-05 23:07:33 -0300

    :tv: add aula 4

---
+++ operacoes.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+echo $idadeHa5Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+//ou
+// $doisAoCubo = 2 * 2 * 2;
+
+$restoDaDivisao = 10 % 3;
+echo $restoDaDivisao;
