79467c5c6cfa66ac27ba71f58bf7c12feaa167c1
Felipe Silva <felipe.dacs@gmail.com>
2021-01-05 23:47:33 -0300

    :tv: add aula 11

--- decisoes.php
+++ decisoes.php
@@ -11,6 +11,7 @@ echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
     echo "Você tem $idade anos. Pode entrar sozinho.";
+// } elseif ($idade >= 16 && $numeroDePessoas > 1) { // tbm funciona
 } else if ($idade >= 16 && $numeroDePessoas > 1) {
     echo "Você tem $idade anos e está acompanhado(a), então pode entrar.";
 } else {
