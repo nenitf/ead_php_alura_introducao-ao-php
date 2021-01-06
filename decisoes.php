452b66e997b30861d2819a301b3f0d4a2a7ec1ca
Felipe Silva <felipe.dacs@gmail.com>
2021-01-05 23:39:16 -0300

    :tv: add aula 9

--- decisoes.php
+++ decisoes.php
@@ -4,10 +4,17 @@ $idade = 17;
 // $idade = 18;
 // $idade = 19;
 
-echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+$numeroDePessoas = 1;
+
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
     echo "Você tem $idade anos." . PHP_EOL;
     echo "Pode entrar";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar";
 }
-echo "Você só tem $idade anos. Você não pode entrar";
+
+echo PHP_EOL;
+echo "Adeus!";
